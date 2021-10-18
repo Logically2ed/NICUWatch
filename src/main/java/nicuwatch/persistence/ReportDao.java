ba47882102088edd4dc0048ae9dc473e40815fc8
blake <blake@blakemcfarlane.com>
2021-10-18 10:10:01 -0400

    sync

--- src/main/java/nicuwatch/persistence/ReportDao.java
+++ src/main/java/nicuwatch/persistence/ReportDao.java
@@ -1,10 +1,14 @@
 package nicuwatch.persistence;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import java.util.List;
+import org.hibernate.Transaction;
+import javax.persistence.criteria.*;
+
 import nicuwatch.entity.Report;
 
 public class ReportDao {
@@ -12,26 +16,75 @@ public class ReportDao {
     private final Logger logger = LogManager.getLogger(this.getClass());
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
+    
+    /** 
+     * @return Session
+     */
     public Session openCurrentSession() {
-        return null; // TODO: add a return
-
+        Session session = sessionFactory.openSession();
+        return session;
     }
 
+    
+    /** 
+     * @param id
+     * @return Report
+     */
     public Report getByReference(int id) {
-        return null;
-        
+        Session session = openCurrentSession();
+        Report report = session.get( Report.class, id );
+        session.close();
+        return report;
     }
+    
+    /** 
+     * @param report
+     */
     public void saveOrUpdate(Report report) {
-
+        Session session = openCurrentSession();
+        session.saveOrUpdate(report);
+        session.close();
     }
+    
+    /** 
+     * @param report
+     * @return int
+     */
     public int insert(Report report){
-        return 0;
+        int id = 0;
+        Session session = openCurrentSession();
+        Transaction transaction = session.beginTransaction();
+        id = (int)session.save(report);
+        transaction.commit();
+        session.close();
+        return id;
     }
+    
+    /** 
+     * @param report
+     */
     public void delete(Report report){
-
+        Session session = openCurrentSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(report);
+        transaction.commit();
+        session.close();
     }
+    
+    /** 
+     * @return List<Report>
+     */
     public List<Report> getAll() {
 
-        return null;
+        Session session = openCurrentSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Report> query = builder.createQuery( Report.class );
+        //Root<Report> root = query.from( Report.class );
+        List<Report> reports = session.createQuery( query ).getResultList();
+
+        logger.debug("The list of users " + reports);
+        session.close();
+
+        return reports;
     }
 }
---
+++ src/test/java/nicuwatch/persistence/ReportDaoTest.java
@@ -0,0 +1,39 @@
+package nicuwatch.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ReportDaoTest {
+    
+    ReportDao dao;
+
+    @BeforeEach
+    void setUp(){
+        dao = new ReportDao();
+    }
+    
+    @Test
+    void testDelete() {
+
+    }
+
+    @Test
+    void testGetAll() {
+
+    }
+
+    @Test
+    void testGetByReference() {
+
+    }
+
+    @Test
+    void testInsert() {
+
+    }
+
+    @Test
+    void testSaveOrUpdate() {
+
+    }
+}
